dbd6696a5e30e64a74b6eb4ac2ababa0dd3a70cd
joshuadaquila <138364264+joshuadaquila@users.noreply.github.com>
2024-08-22 13:00:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
@@ -9,13 +8,12 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-// MySQL connection
+// MySQL connection using Unix socket
 const pool = mysql.createPool({
-  host: '34.70.44.155',
+  socketPath: '/cloudsql/barkota-1143:us-central1:barkota-db-testing',
   user: 'barkotafaq',
   password: 'vmJKx0teRd',
-  database: 'barkotafaq',
-  port: 3307
+  database: 'barkotafaq'
 });
 
 // Example endpoint
