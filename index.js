c2f133db2c6e40d0625867f8216473541450f29c
joshuadaquila <138364264+joshuadaquila@users.noreply.github.com>
2024-08-22 10:27:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // MySQL connection
 const pool = mysql.createPool({
-  host: 'barkota-1143:us-central1:barkota-db-testing',
+  host: '34.70.44.155',
   user: 'barkotafaq',
   password: 'vmJKx0teRd',
   database: 'barkotafaq',
